327f8a0ee5e668f5850d459b14332a31d7405115
Henry Zhang <hzhang20902@gmail.com>
2024-01-05 16:10:43 -0500

    spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
   );
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <React.Fragment>
         <Button onClick={toggleDrawer('menu', true)} sx={{"&:hover": {backgroundColor: "transparent", }}} className='menu' disableFocusRipple disableRipple>
